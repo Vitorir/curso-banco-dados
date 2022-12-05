0a03989230561c38a8b7dbd0058de44119062e2a
Vitorir <vitorireboucas@edu.unifor.br>
2022-12-05 19:05:03 -0300

    Subindo alteracoes do pc

--- BD_atv_final.sql
+++ BD_atv_final.sql
@@ -4,61 +4,71 @@ use ControleDeBens;
 select * from bem;
 select * from usuario;
 select * from historico;
+select * from bem_has_usuario;
 
-drop table bem;
+desc historico;
+desc bem;
+desc usuario;
+desc bem_has_usuario;
+
+drop database controledebens;
 
 create table usuario (
 idUsuario int not null primary key auto_increment,
 nome varchar(45) not null,
-percentualBem decimal(10,2),
 valorPatrimonial decimal(10,2) 
 );
 
 create table bem (
 idBem int not null primary key auto_increment,
+idUsuario int,
 nomeBem varchar(45) not null,
 dataCompra date,
 valor decimal(10, 2),
 tipoBem varchar(45),
-desgaste enum('V', 'F'),
+desgaste char(1),
 dataValidade date,
-valorSentimental varchar(45)
+taxaValorizacao decimal(10, 2)
 );
-
-alter table bem
-add column taxaValorizacao decimal(10, 2) after desgaste;
 
 create table bem_has_usuario (
   bem_idBem int not null,
+  nomeBem varchar(45),
   usuario_idUsuario int not null,
-  percentualBem decimal(3,2) not null,
-  totalInvestido varchar(45) not null,
+  nomeUsuario varchar(45),
+  percentualBem decimal(10, 2),
+  totalInvestido decimal(10,2),
+  valorSentimental varchar(45),
   primary key(bem_idBem, usuario_idUsuario),
-  index fk_bem_has_usuario_usuario (usuario_idUsuario),
-  index fk_bem_has_usuario_bem (bem_idBem),
-  constraint fk_bem_has_usuario
+  index fk_bem_has_usuario_usuario(usuario_idUsuario, bem_idBem)
+);
+
+
+
+alter table bem_has_usuario
+  add constraint fk_bem_has_usuario
     foreign key(bem_idBem)
     references bem(idBem),
-  constraint fk_bem_has_usuario_usuario
+  add constraint fk_bem_has_usuario_usuario
     foreign key(usuario_idUsuario)
-    references usuario(idUsuario)
-);
+    references usuario(idUsuario);
+
 
 create table historico (
 idHistorico int not null primary key auto_increment,
 idBem int,
 nomeBem varchar(45) not null,
 dataEspecifica date not null,
-valor decimal(10,2)not null
+valor decimal(10,2) not null
 );
 
 alter table bem 
-add constraint fk_id_usuario 
+add constraint fk_idUsuario 
 foreign key(idUsuario)
-references usuario (idUsuario);
+references usuario(idUsuario);
 
 
 alter table historico 
-add constraint fk_id_bem 
+add constraint fk_idBem 
 foreign key (idBem)
-references bem (idBem);
+references bem(idBem);
